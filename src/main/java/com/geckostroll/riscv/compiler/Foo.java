70397c0e5b162a7c11f510f661b7f27f30e7feb4
yanhuai.yh <yanhuai.yh@antgroup.com>
2022-01-26 11:39:03 +0800

    first commit

---
+++ src/main/java/com/geckostroll/riscv/compiler/Foo.java
@@ -0,0 +1,20 @@
+/*
+ * Ant Group
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package com.geckostroll.riscv.compiler;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+/**
+ * @author yan
+ * @version Foo.java, v 0.1 2022年01月26日 1:24 AM yan
+ */
+public class Foo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Compiler!");
+        System.out.println(Joiner.on(',').join(Lists.newArrayList("abc", "cde")));
+    }
+}
